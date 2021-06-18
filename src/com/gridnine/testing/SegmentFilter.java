5e935850b34614cec9a887832aadbb710ec1e4f9
Diana <basss_1993@list.ru>
2021-06-19 03:08:13 +0500

    create methods

--- src/com/gridnine/testing/SegmentFilter.java
+++ src/com/gridnine/testing/SegmentFilter.java
@@ -1,4 +1,14 @@
 package com.gridnine.testing;
 
+import java.util.List;
+import java.util.Set;
+
 public interface SegmentFilter {
+    void showAllSegments(List<Flight> flightList);
+
+    Set<Flight> getDepartureTimeBeforeNow(List<Flight> flightList);
+
+    Set<Flight> getArrivalDateLessDepartureDate(List<Flight> flightList);
+
+    Set<Flight> getFlightWithTransferMoreThanTwoHours(List<Flight> flightList);
 }
