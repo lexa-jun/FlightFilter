55ad1634c0d6934d0747096c01f541ca573bcecc
Diana <basss_1993@list.ru>
2021-06-19 01:27:50 +0500

    class Flight
    create getId()
    
    create getSegments()
    
    create String toString()

--- src/com/gridnine/testing/Flight.java
+++ src/com/gridnine/testing/Flight.java
@@ -1,15 +1,32 @@
 package com.gridnine.testing;
 
+import java.beans.IntrospectionException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flight {
 
     private final Integer id;
     private final List<Segment> segments;
 
+    public Integer getId(){
+        return id;
+    }
+
+    List<Segment> getSegments(){
+        return segments;
+    }
+
     Flight(final List<Segment> segs, Integer id){
         segments = segs;
         this.id = id;
     }
+    @Override
+    public String toString(){
+        return segments.stream().map(Object::toString).collect(Collectors.joining(" "));
+    }
+
+
+
 }
 
