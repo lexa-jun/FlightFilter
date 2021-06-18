ef2088642ac3af31f437ebf94aa128118a44afab
Diana <basss_1993@list.ru>
2021-06-19 01:20:20 +0500

    create class Segment { create variables id, departureDate, arrivalDate; } create Segment (){
    }

--- src/com/gridnine/testing/Flight.java
+++ src/com/gridnine/testing/Flight.java
@@ -1,7 +1,15 @@
 package com.gridnine.testing;
 
-
+import java.util.List;
 
 public class Flight {
 
+    private final Integer id;
+    private final List<Segment> segments;
+
+    Flight(final List<Segment> segs, Integer id){
+        segments = segs;
+        this.id = id;
+    }
 }
+
--- src/com/gridnine/testing/FlightBuilder.java
+++ src/com/gridnine/testing/FlightBuilder.java
@@ -1,10 +1,12 @@
 package com.gridnine.testing;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FlightBuilder {
+
     private Integer flightId = 0;
     private Integer segmentId = 0;
 
@@ -28,6 +30,19 @@ public class FlightBuilder {
                         threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4),
                         threeDaysFromNow.plusHours(6), threeDaysFromNow.plusHours(7)));
 
+    }
+    protected Flight createFlight(final LocalDateTime... dates) {
+        if ((dates.length % 2) != 0) {
+            throw new IllegalArgumentException(
+                    "пожалуйста, используйте четное количество дней");
+        }
+        List<Segment> segments = new ArrayList<>(dates.length / 2);
+        for (int i = 0; i < (dates.length - 1); i += 2) {
+            segmentId += 1;
+            segments.add(new Segment (dates[i], dates[i + 1], segmentId));
+        }
+        flightId += 1;
+        return new Flight(segments, flightId);
     }
 
 }
