8941e02fe21e9bee20d2e3a9167911311cff0e75
Diana <basss_1993@list.ru>
2021-06-19 01:31:52 +0500

    class Segment
    create getId()
    
    create getDepartureDate()
    
    create getArrivalDate()
    
    create String toString()

---
+++ src/com/gridnine/testing/Segment.java
@@ -0,0 +1,39 @@
+package com.gridnine.testing;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Segment {
+
+    private final Integer id;
+    private final LocalDateTime departureDate;
+    private final LocalDateTime arrivalDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    LocalDateTime getDepartureDate() {
+        return departureDate;
+    }
+
+    LocalDateTime getArrivalDate() {
+        return arrivalDate;
+    }
+
+    Segment(final LocalDateTime departure, final LocalDateTime arrival, Integer id) {
+        departureDate = Objects.requireNonNull(departure);
+        arrivalDate = Objects.requireNonNull(arrival);
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter fmt =
+                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        return '[' + departureDate.format(fmt) + '|' + arrivalDate.format(fmt)
+                + ']';
+    }
+
+}
