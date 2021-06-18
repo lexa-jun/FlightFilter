b85211c9f50725f0dfd07b1c24568f1e238ec6ed
Diana <basss_1993@list.ru>
2021-06-19 02:01:41 +0500

    create void showAllSegments

--- src/com/gridnine/testing/Main.java
+++ src/com/gridnine/testing/Main.java
@@ -1,5 +1,7 @@
 package com.gridnine.testing;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
--- src/com/gridnine/testing/SegmentFilterImpl.java
+++ src/com/gridnine/testing/SegmentFilterImpl.java
@@ -1,4 +1,18 @@
 package com.gridnine.testing;
 
+import java.util.List;
+
 public class SegmentFilterImpl implements SegmentFilter {
+
+
+    public void showAllSegments(List<Flight> flights) {
+        for (Flight flight : flights) {
+            System.out.println("Номер рейса - " + flight.getId());
+            for (int i = 0; i < flight.getSegments().size(); i++) {
+                System.out.println(flight.getSegments().get(i)
+                        + " Номер сегмента: "
+                        + flight.getSegments().get(i).getId());
+            }
+        }
+    }
 }
