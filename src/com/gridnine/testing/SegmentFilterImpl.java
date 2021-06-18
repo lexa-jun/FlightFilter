3dab9c81070a41c92ef8c7ff32d499ebd1fbba63
Diana <basss_1993@list.ru>
2021-06-19 02:15:12 +0500

    create void showRightFlight
    create variable DateTimeFormatter

--- src/com/gridnine/testing/SegmentFilterImpl.java
+++ src/com/gridnine/testing/SegmentFilterImpl.java
@@ -1,9 +1,17 @@
 package com.gridnine.testing;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class SegmentFilterImpl implements SegmentFilter {
 
+    private DateTimeFormatter getDateFormatter() {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd' T 'HH:mm");
+    }
 
     public void showAllSegments(List<Flight> flights) {
         for (Flight flight : flights) {
@@ -15,4 +23,15 @@ public class SegmentFilterImpl implements SegmentFilter {
             }
         }
     }
+
+    private void showRightFLight(Flight flight, LocalDateTime departureTime, LocalDateTime arrivalTime) {
+        DateTimeFormatter dateTimeFormatter = getDateFormatter();
+        System.out.println("Номер рейса - " + flight.getId() + "\n" + "Время отправления: "
+                + dateTimeFormatter.format(departureTime) + "\n" + "Время прибытия: "
+                + dateTimeFormatter.format(arrivalTime));
+        System.out.println("..................................");
+
+    }
+
+
 }
