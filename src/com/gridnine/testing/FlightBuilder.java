0d6b48f1dfc37216e81ba56269fdc48c3f40e9c2
Diana <basss_1993@list.ru>
2021-06-19 00:29:32 +0500

    create List<Flight>{ create Arrays.asList( createFlight (Duration division, multi-segment, performed in the past, departures earlier than arrivals, waiting on the ground for more than 2 hours, waiting on the ground for 2 hours; ) } create class Flight

--- src/com/gridnine/testing/FlightBuilder.java
+++ src/com/gridnine/testing/FlightBuilder.java
@@ -1,7 +1,33 @@
 package com.gridnine.testing;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 public class FlightBuilder {
     private Integer flightId = 0;
     private Integer segmentId = 0;
+
+    public List<Flight> createFlights() {
+        LocalDateTime threeDaysFromNow = LocalDateTime.now().plusDays(3);
+        return Arrays.asList(
+                // Обычный рейс продолжительностью 2 часа
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2)),
+                // Обычный мульти-сегментный рейс
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
+                        threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(5)),
+                // рейс, выполнявшийся в прошлом
+                createFlight(threeDaysFromNow.minusDays(6), threeDaysFromNow),
+                // вылет раньше, чем прибытие
+                createFlight(threeDaysFromNow, threeDaysFromNow.minusHours(6)),
+                // рейс с ожиданием на земле более 2 часов
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
+                        threeDaysFromNow.plusHours(5), threeDaysFromNow.plusHours(6)),
+                // рейс с ожиданием на земле 2 часа
+                createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
+                        threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4),
+                        threeDaysFromNow.plusHours(6), threeDaysFromNow.plusHours(7)));
+
+    }
 
 }
